f2f90a4bf3d16fe79f0dde651468a74b840ac95d
Tanjil Ahamed <tanjil113355@gmail.com>
2024-02-01 11:48:23 +0600

    banner section complete

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,24 @@
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/p43bQh3/toni-lluch-Jo-Roy500n-Cc-unsplash.jpg)' }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content flex-col-reverse md:flex-row justify-center text-neutral-content">
+                <div className=" md:w-1/2  text-white">
+                    <div className="mb-3">
+                        <p>Top Trending</p>
+                        <hr className="border-2 w-[90px] mt-1 " />
+                    </div>
+                    <h1 className="mb-5 text-5xl font-bold leading-[60px]">
+                        QuadB: Where Cinematic <span className="text-blue-200">Worlds Collide</span></h1>
+                    <p className="mb-5">QuadB is your ultimate destination for all things cinema. Dive into a vast library of movies, explore curated lists, and engage with fellow film enthusiasts. With reviews, recommendations, and exclusive content, QuadB is your ticket to cinematic bliss.</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+                <div className="md:w-1/2">
+                    <img className="w-full" src="https://i.ibb.co/w4k0kyC/pngwing-com-3.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,13 @@
+import Banner from "../Components/Banner";
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
+
 const Home = () => {
     return (
         <div>
-            
+            <Navbar />
+            <Banner />
+            <Footer />
         </div>
     );
 };
